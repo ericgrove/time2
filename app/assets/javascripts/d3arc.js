f0ca094ecd8f703b2d47e574a7629702d7c065ba
Eric Grove <odin36035@gmail.com>
2014-12-21 20:40:00 -0500

    third try

--- app/assets/javascripts/d3arc.js
+++ app/assets/javascripts/d3arc.js
@@ -326,7 +326,7 @@ var svg = d3.select("#clock")
 					var marwk6path = marCircle.append("path")
 						.attr("d", marwk6arc)
 						.attr("class", "weeks");
-// change for git test
+// change for git test a second time
 
 		// apr circle
 			var aprCircle = yearCircle.append("g")
